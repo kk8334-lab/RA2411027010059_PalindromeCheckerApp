1910b2233ad10e219534c051a2c09185e201c388
kk8334-lab <kk8334@srmist.edu.in>
2026-02-24 15:58:07 +0530

    Add UseCase2 PalindromeCheckerApp

---
+++ src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,34 @@
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        System.out.println("====================================");
+        System.out.println(" Welcome to Palindrome Checker App - UC2 ");
+        System.out.println("====================================");
+
+        String word = "madam";
+
+        boolean isPalindrome = checkPalindrome(word);
+
+        if (isPalindrome) {
+            System.out.println("The word \"" + word + "\" is a palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a palindrome.");
+        }
+
+        System.out.println("Exiting UC2...");
+    }
+
+    public static boolean checkPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
